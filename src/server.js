c3d77636885c3e3f9203916c918def0d00c7b032
ntdinh <nguyentiendinh1999@gmail.com>
2021-02-02 16:02:16 +0700

    [1.0.0] add static page

--- src/server.js
+++ src/server.js
@@ -1,26 +1,21 @@
 import express  from"express";
 import ConnectDB from "./config/connectDB";
-import ContactModel from "./models/contact.model";
+import configViewEngine from "./config/viewEngine";
 
 let app = express();
 //Con ket qua mongodb
 ConnectDB();
 
- 
+//** config */
 
-app.get("/test-data", async (req,res)=>{
-    try {
-        let item = {
-            userId :"12565",
-            contactId : "147996",
-        };
-        
-        let contact = await ContactModel.createNew(item);
-        res.send(contact);
-    } catch (error) {
-       console.log(error); 
-    }
-   
+ configViewEngine(app);
+
+app.get("/",   (req,res)=>{
+    return res.render("main/master");
+});
+
+app.get("/login-register",   (req,res)=>{
+    return res.render("auth/loginRegister");
 });
 
 app.listen(process.env.APP_PORT,process.env.APP_HOST,()=>{
